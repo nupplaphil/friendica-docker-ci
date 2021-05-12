0373dc253adb71957bfdae054dd8183bc3b23d86
Philipp <admin@philipp.info>
2021-05-12 19:11:42 +0200

    Add friendicaci/transifex image

---
+++ transifex/check-messages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Checks the default messages.po file
+#
+
+# Skip first 4 lines in possible diff, because they're header
+# Skip all lines of the git diff starting with "@@" or comments or starting "POT-Creation-Date"
+if [[ $(git diff -U0 ./view/lang/C/messages.po | grep -vE '^(@@|\-"POT-Creation-Date|\+"POT-Creation-Date|\-#|\+#)' | wc -l) -gt 4 ]]; then
+	echo "$(git diff ./view/lang/C/messages.po)"
+	exit 1
+else
+	echo "Nothing to update"
+fi
